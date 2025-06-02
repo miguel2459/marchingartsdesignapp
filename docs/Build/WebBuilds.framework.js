d055c529e53373a4b49ca86f2964f3a2ded98bba
miguel2459 <miguelrodriquez45@gmail.com>
2025-06-01 17:14:56 -0700

    Deploy fresh Unity WebGL build to GitHub Pages

--- docs/Build/WebBuilds.framework.js
+++ docs/Build/WebBuilds.framework.js
@@ -1284,10 +1284,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  5666888: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 5666943: ($0) => { performance.now = function() { return $0; }; },  
- 5666991: ($0) => { performance.now = function() { return $0; }; },  
- 5667039: () => { performance.now = Module['emscripten_get_now_backup']; }
+  5666744: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 5666799: ($0) => { performance.now = function() { return $0; }; },  
+ 5666847: ($0) => { performance.now = function() { return $0; }; },  
+ 5666895: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
