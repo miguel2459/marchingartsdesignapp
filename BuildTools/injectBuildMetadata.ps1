2f68ecd01f4f79489e82ce9dc451a837fa7a2924
miguel2459 <miguelrodriquez45@gmail.com>
2025-06-19 22:20:28 -0700

    working on reloading same build cache

--- BuildTools/injectBuildMetadata.ps1
+++ BuildTools/injectBuildMetadata.ps1
@@ -50,7 +50,7 @@ $html = $html -replace '<script src="Build/.*?\.loader\.js"></script>', "<script
 
 # Replace file references inside createUnityInstance
 $html = $html `
-    -replace 'dataUrl:\s*"Build/.*?\.data\?v=.*?(&cb=\$\{Date\.now\(\)\})"', "dataUrl: `"Build/$($dataFile.Name)?v=$timestamp`$1`"" `
+    -replace 'dataUrl:\s*"Build/.*?\.data\?v=.*?"\s*\+\s*Date\.now\(\)', "dataUrl: `"Build/$($dataFile.Name)?v=$timestamp&cb=`" + Date.now()" `
     -replace 'frameworkUrl:\s*".*?"', "frameworkUrl: `"Build/$($frameworkFile.Name)?v=$timestamp`"" `
     -replace 'codeUrl:\s*".*?"', "codeUrl: `"Build/$($wasmFile.Name)?v=$timestamp`""
 
