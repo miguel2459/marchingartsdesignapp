1bcc49491bf1faca44305c2a4ebc0e0aeb87f6bd
miguel2459 <miguelrodriquez45@gmail.com>
2025-06-24 18:33:46 -0700

    restoring ScrollAndPinch.cs to handle touch camera movemements

--- Assets/Scripts/CameraSystem/FlyingCameraController.cs
+++ Assets/Scripts/CameraSystem/FlyingCameraController.cs
@@ -47,7 +47,7 @@ public class FlyingCameraController : MonoBehaviour, ICameraFocusHandler
     void Update()
     {
         if (!isActive) return;
-        //if (isMobile) return; // 🚫 Skip keyboard/mouse input on mobile
+        if (isMobile) return; // 🚫 Skip keyboard/mouse input on mobile
 
         if (isFocusing)
         {
